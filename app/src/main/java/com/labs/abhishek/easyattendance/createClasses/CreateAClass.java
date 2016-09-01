24fff52ad98b6bfea958b3d48c2c50bc8d6570ae
abhishekhere <anand007abhishek@gmail.com>
2016-09-01 09:17:16 +0530

    Changes till: BackToDashboard and AddMembersButtons Added; Spinner toBeDone

--- app/src/main/java/com/labs/abhishek/easyattendance/createClasses/CreateAClass.java
+++ app/src/main/java/com/labs/abhishek/easyattendance/createClasses/CreateAClass.java
@@ -1,12 +1,16 @@
 package com.labs.abhishek.easyattendance.createClasses;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.labs.abhishek.easyattendance.NavigationPage;
 import com.labs.abhishek.easyattendance.R;
 import com.labs.abhishek.easyattendance.dbConnection.ClassTableDBHelper;
 
@@ -15,16 +19,22 @@ import com.labs.abhishek.easyattendance.dbConnection.ClassTableDBHelper;
  */
 public class CreateAClass extends AppCompatActivity {
 
-    Button bcreateAClass;
+    Button bcreateAClass, bAddMembers, bBackToDashboard;
     EditText etClassName;
+    TextView tvCeilingMessage;
+    Spinner spinnerClassMenu;
     String className;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_class);
+        tvCeilingMessage = (TextView) findViewById(R.id.tvCeilingMessage);
         bcreateAClass = (Button) findViewById(R.id.bAddClass);
+        bAddMembers = (Button) findViewById(R.id.bAddMembers);
+        bBackToDashboard = (Button) findViewById(R.id.bBackToDashboard);
         etClassName = (EditText) findViewById(R.id.etClassToBeAdded);
+        spinnerClassMenu = (Spinner) findViewById(R.id.spinnerClassMenu);
 
         bcreateAClass.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -33,6 +43,12 @@ public class CreateAClass extends AppCompatActivity {
             }
         });
 
+        bBackToDashboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToDashboard();
+            }
+        });
     }
 
     private void insertClassIntoDB(String className){
@@ -48,5 +64,27 @@ public class CreateAClass extends AppCompatActivity {
         if(result){
             Log.i("## Success"," in inserting class");
         }
+        if (result) {
+            tvCeilingMessage.setText("Add members to " + className);
+            //spinnerClassMenu.set
+            spinnerClassMenu.setVisibility(View.VISIBLE);
+            bAddMembers.setVisibility(View.VISIBLE);
+            bBackToDashboard.setVisibility(View.VISIBLE);
+            etClassName.setVisibility(View.INVISIBLE);
+            bcreateAClass.setVisibility(View.INVISIBLE);
+        } else {
+            bcreateAClass.setText("Something went wrong : Try Again");
+        }
+    }
+
+    private void goToDashboard() {
+        final Intent intentDashBoard = new Intent(this, NavigationPage.class);
+        Thread threadGoToDashboard = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intentDashBoard);
+            }
+        });
+        threadGoToDashboard.start();
     }
 }
